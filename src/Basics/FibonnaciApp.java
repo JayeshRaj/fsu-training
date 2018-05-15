539161d2a5841d10d4ec11310d7b47e3358bb491
Jayesh <jayeshraj.agrawal@gmail.com>
2018-05-15 19:08:55 -0400

    again

--- src/Basics/FibonnaciApp.java
+++ src/Basics/FibonnaciApp.java
@@ -5,6 +5,7 @@ public class FibonnaciApp {
 	public static void main(String[] args) {
 		System.out.println("program is starting ");
 		System.out.println(fib(9));
+		System.out.println("program ended ");
 	}
 public static int fib (int n) {
 	if(n == 0) {
@@ -16,6 +17,7 @@ else if (n == 1 ){
 	return 1 ;
 }
 return (fib (n-1) + fib (n-2));
-System.out.println("program ended ");
+
 }
 }
+
